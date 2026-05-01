912393b8042347298751c1ed1f1e9fd06fcde8a0
Shakib Khan <leadwithshakib@gmail.com>
2026-05-01 11:22:56 +0600

    refactor: update lib/env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -1,8 +1,4 @@
-export const CLOUDFLARE_API_KEY = process.env.CLOUDFLARE_API_KEY;
-export const GLM_WORKER_URL = process.env.GLM_WORKER_URL;
-
-export const CLOUDFLARE_AI_GATEWAY_API_KEY = process.env.CLOUDFLARE_AI_GATEWAY_API_KEY;
-export const CLOUDFLARE_AI_GATEWAY_ENDPOINT = process.env.CLOUDFLARE_AI_GATEWAY_ENDPOINT;
+export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 export const AWS_REGION = process.env.AWS_REGION;
 export const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
